6379d4b4e184a92c3aa1114d9529a5905a8f63be
mazharul90007 <mazharul90007@gmail.com>
2025-04-27 19:32:42 +0600

    Testimonial section update

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
+//Testimonial function start
+document.querySelector('.load-more-btn').addEventListener('click', function () {
+  const grid = document.querySelector('.testimonial-grid');
+  const button = this;
+
+  // Toggle active state
+  grid.classList.toggle('active');
+
+  // Change button text
+  if (grid.classList.contains('active')) {
+    button.textContent = 'Show Less';
+  } else {
+    button.textContent = 'Load More +';
+  }
+});
+//Testimonial function ends
+
 // Faq section functions start
 const faqItems = document.querySelectorAll('.faq-item');
 
 faqItems.forEach(item => {
   const question = item.querySelector('.faq-question-container');
   question.addEventListener('click', () => {
-    // Toggle this item
     item.classList.toggle('active');
 
-    // Collapse others
     faqItems.forEach(other => {
       if (other !== item) {
         other.classList.remove('active');
